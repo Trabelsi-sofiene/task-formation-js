af5f4ff380acef494da9c371a02dea7e0a8f4d58
Trabelsi sofiene <120690918+Trabelsi-sofiene@users.noreply.github.com>
2023-02-02 14:41:41 +0100

    Delete task4.js

--- task4.js
+++
@@ -1,7 +0,0 @@
-function telephoneCheck(phoneNumber) {
-    const pattern = /^\d{10}$|^(\d{3}-){2}\d{4}$|^(\d{3}){1} \d{3}-\d{4}$/;
-    return pattern.test(phoneNumber);
-  }
-  console.log(telephoneCheck('555-555-5555'))
-
-
